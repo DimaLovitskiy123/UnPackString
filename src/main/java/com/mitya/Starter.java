51e18eec948b0de2b9b507cb14a6e65f6881b281
ДИМА <dimlovitskiy@gmail.com>
2021-11-28 12:59:46 +0300

    2 сommit

--- src/main/java/com/mitya/Starter.java
+++ src/main/java/com/mitya/Starter.java
@@ -3,8 +3,9 @@ package com.mitya;
 public class Starter {
     public static void main(String[] args) {
 
+        System.out.println("Программа началась.");
         UnPacker up = new UnPacker();
         up.returnString();
-        System.out.println("Программа закончена");
+        System.out.println("Программа закончена.");
     }
 }
